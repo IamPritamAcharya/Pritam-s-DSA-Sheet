df29ff7607deed40659c46aba20a25b4069db57a
Aeros <pritamach.exe@gmail.com>
2026-03-30 22:27:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/Pritam-s-DSA-Sheet/',
+  base: '/SolveStack/', 
   plugins: [react()],
 })
